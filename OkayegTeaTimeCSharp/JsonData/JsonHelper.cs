670db7e1bd9e3d4bad33082a33cf0bf1a1a70dfc
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-28 18:39:30 +0200

    jsonhelper methods added

--- OkayegTeaTimeCSharp/JsonData/JsonHelper.cs
+++ OkayegTeaTimeCSharp/JsonData/JsonHelper.cs
@@ -1,6 +1,22 @@
-﻿namespace OkayegTeaTimeCSharp.JsonData
+﻿using System.Text.Json;
+using OkayegTeaTimeCSharp.JsonData.JsonClasses;
+
+namespace OkayegTeaTimeCSharp.JsonData
 {
     public static class JsonHelper
     {
+        public static string Serialize(Data data)
+        {
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true
+            };
+            return JsonSerializer.Serialize(data, options);
+        }
+
+        public static Data Deserialize(string json)
+        {
+            return JsonSerializer.Deserialize<Data>(json);
+        }
     }
 }
