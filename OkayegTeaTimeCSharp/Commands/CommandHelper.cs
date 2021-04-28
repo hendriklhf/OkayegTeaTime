4a8e546bbab5d2d7a8404083e7fb803c752aadb7
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-28 23:51:56 +0200

    command helper added

---
+++ OkayegTeaTimeCSharp/Commands/CommandHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OkayegTeaTimeCSharp.JsonData;
+using OkayegTeaTimeCSharp.JsonData.JsonClasses;
+
+namespace OkayegTeaTimeCSharp.Commands
+{
+    public static class CommandHelper
+    {
+        public static Command GetCommand(string name)
+        {
+            return JsonHelper.JsonToObject().CommandLists.Commands.Where(cmd => cmd.Alias.Any(alias => alias == name.ToLower())).FirstOrDefault();
+        }
+
+        public static AfkCommand GetAfkCommand(string name)
+        {
+            return JsonHelper.JsonToObject().CommandLists.AfkCommands.Where(cmd => cmd.Alias.Any(alias => alias == name.ToLower())).FirstOrDefault();
+        }
+    }
+}
