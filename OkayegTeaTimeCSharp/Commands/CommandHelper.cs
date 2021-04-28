9ff29f6c843a834706b3f7903453fbd8f72325d9
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-29 01:14:58 +0200

    code cleaning

--- OkayegTeaTimeCSharp/Commands/CommandHelper.cs
+++ OkayegTeaTimeCSharp/Commands/CommandHelper.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OkayegTeaTimeCSharp.JsonData;
+﻿using OkayegTeaTimeCSharp.JsonData;
 using OkayegTeaTimeCSharp.JsonData.JsonClasses;
+using System.Linq;
 
 namespace OkayegTeaTimeCSharp.Commands
 {
