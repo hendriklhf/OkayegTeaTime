dbb8c6ba80e4e23641883748772baa01783bf9d1
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-29 10:35:30 +0200

    ping command class created

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/Ping.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public static class Ping
+    {
+    }
+}
