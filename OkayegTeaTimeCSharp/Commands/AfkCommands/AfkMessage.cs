d79b56ea681640610b516b7cc88d89401dd6d5ec
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-29 17:59:35 +0200

    class and enum for afkcommand created

---
+++ OkayegTeaTimeCSharp/Commands/AfkCommands/AfkMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace OkayegTeaTimeCSharp.Commands.AfkCommands
+{
+    public class AfkMessage
+    {
+        public string ComingBack { get; private set; }
+
+        public string GoingAway { get; private set; }
+
+        private AfkMessage(string comingBack, string goingAway)
+        {
+            ComingBack = comingBack;
+            GoingAway = goingAway;
+        }
+
+#warning maybe username as parameter
+        public static AfkMessage Create(string afkCommandName)
+        {
+            return new AfkMessage(CommandHelper.GetAfkCommand(afkCommandName).ComingBack, CommandHelper.GetAfkCommand(afkCommandName).GoingAway);
+        }
+    }
+}
---
+++ OkayegTeaTimeCSharp/Commands/AfkCommands/Enumeration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OkayegTeaTimeCSharp.Commands.AfkCommands
+{
+#warning braucht klasse die erbt
+    public class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+
+        public AfkMessage Message { get; private set; }
+
+        protected Enumeration(string name, AfkMessage message)
+        {
+            Name = name;
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Name.CompareTo(((Enumeration)obj).Name);
+        }
+    }
+}
