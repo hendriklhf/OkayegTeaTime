55ace0171a52e1162af6d6e898b49642c2ab4286
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-29 03:15:24 +0200

    added methods to check if message is command

--- OkayegTeaTimeCSharp/Commands/CommandHelper.cs
+++ OkayegTeaTimeCSharp/Commands/CommandHelper.cs
@@ -1,5 +1,6 @@
 ﻿using OkayegTeaTimeCSharp.JsonData;
 using OkayegTeaTimeCSharp.JsonData.JsonClasses;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OkayegTeaTimeCSharp.Commands
@@ -14,6 +15,37 @@ namespace OkayegTeaTimeCSharp.Commands
         public static AfkCommand GetAfkCommand(string name)
         {
             return JsonHelper.JsonToObject().CommandLists.AfkCommands.Where(cmd => cmd.Alias.Any(alias => alias == name.ToLower())).FirstOrDefault();
+        }
+
+        public static List<string> GetCommandAliases()
+        {
+            List<string> listAlias = new();
+            JsonHelper.JsonToObject().CommandLists.Commands.ForEach(cmd =>
+            {
+                cmd.Alias.ForEach(alias =>
+                {
+                    listAlias.Add(alias);
+                });
+            });
+            return listAlias;
+        }
+
+        public static List<string> GetAfkCommandAliases()
+        {
+            List<string> listAlias = new();
+            JsonHelper.JsonToObject().CommandLists.AfkCommands.ForEach(cmd =>
+            {
+                cmd.Alias.ForEach(alias =>
+                {
+                    listAlias.Add(alias);
+                });
+            });
+            return listAlias;
+        }
+
+        public static List<string> GetAllAliases()
+        {
+            return GetCommandAliases().Concat(GetAfkCommandAliases()).ToList();
         }
     }
 }
