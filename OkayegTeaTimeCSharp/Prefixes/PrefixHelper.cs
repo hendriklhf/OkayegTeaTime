357cdc4d31efd598da0d056b664acbcce5bf555c
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-29 11:46:40 +0200

    prefixhelper added

---
+++ OkayegTeaTimeCSharp/Prefixes/PrefixHelper.cs
@@ -0,0 +1,25 @@
+﻿using OkayegTeaTimeCSharp.Database;
+using System.Collections.Generic;
+
+namespace OkayegTeaTimeCSharp.Prefixes
+{
+    public static class PrefixHelper
+    {
+        public static Dictionary<string, string> Prefixes { get; private set; } = new();
+
+        public static void FillDictionary()
+        {
+            Prefixes = DataBase.GetPrefixes();
+        }
+
+        public static void Update(string channel)
+        {
+            Prefixes[$"#{channel}"] = DataBase.GetPrefix(channel);
+        }
+
+        public static string GetPrefix(string channel)
+        {
+            return Prefixes[$"#{channel}"] ?? "";
+        }
+    }
+}
