e15595e47ce1c5c6e644c4e6a0cda16b039d6bb3
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-30 04:09:41 +0200

    added DiscordClient

---
+++ OkayegTeaTimeCSharp/Discord/DiscordClient.cs
@@ -0,0 +1,14 @@
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using System;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Discord
+{
+    public static class DiscordClient
+    {
+        public static void SendDiscordMessageIfAFK(TwitchBot twitchBot, ChatMessage chatMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
