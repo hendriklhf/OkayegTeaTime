ec3a68c2bf0c7c0fe5c700616e0ff3aedb42509b
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-30 02:10:14 +0200

    added helper functions for chat messages

---
+++ OkayegTeaTimeCSharp/Twitch/TwitchLibHelper.cs
@@ -0,0 +1,23 @@
+﻿using OkayegTeaTimeCSharp.Messages;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Twitch
+{
+    public static class TwitchLibHelper
+    {
+        public static string GetMessage(this ChatMessage chatMessage)
+        {
+            return chatMessage.Message.MakeUsable();
+        }
+
+        public static string[] GetSplit(this ChatMessage chatMessage)
+        {
+            return chatMessage.Message.Split();
+        }
+
+        public static string[] GetLowerSplit(this ChatMessage chatMessage)
+        {
+            return chatMessage.Message.SplitToLowerCase();
+        }
+    }
+}
