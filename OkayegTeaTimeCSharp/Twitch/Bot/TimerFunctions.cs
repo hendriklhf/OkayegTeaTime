a929cff8925e6150802209c839554a8ad148e0ab
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-04-30 04:09:56 +0200

    added own class for Timer functions

---
+++ OkayegTeaTimeCSharp/Twitch/Bot/TimerFunctions.cs
@@ -0,0 +1,18 @@
+﻿using OkayegTeaTimeCSharp.Database;
+using System;
+
+namespace OkayegTeaTimeCSharp.Twitch.Bot
+{
+    public static class TimerFunctions
+    {
+        public static void CheckForTimedReminders(TwitchBot twitchBot)
+        {
+            DataBase.CheckForTimedReminder(twitchBot);
+        }
+
+        public static void BanSecretChatUsers(TwitchBot twitchBot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
