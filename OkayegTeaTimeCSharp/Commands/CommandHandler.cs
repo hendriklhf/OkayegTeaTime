246274df1e1e19772d85138e79144f2abd36bab4
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 21:44:02 +0200

    renamed methods

--- OkayegTeaTimeCSharp/Commands/CommandHandler.cs
+++ OkayegTeaTimeCSharp/Commands/CommandHandler.cs
@@ -26,7 +26,7 @@ namespace OkayegTeaTimeCSharp.Commands
                             {
                                 if (chatMessage.GetMessage().IsMatch(PatternCreator.Create(alias, PrefixType.None)))
                                 {
-                                    Type.GetType(CommandHelper.GetClassName(type)).GetMethod("Handle").Invoke(null, new object[] { twitchBot, chatMessage });
+                                    Type.GetType(CommandHelper.GetCommandClassName(type)).GetMethod("Handle").Invoke(null, new object[] { twitchBot, chatMessage });
                                 }
                             });
                         }
@@ -36,7 +36,7 @@ namespace OkayegTeaTimeCSharp.Commands
                             {
                                 if (chatMessage.GetMessage().IsMatch(PatternCreator.Create(alias, PrefixType.Active)))
                                 {
-                                    Type.GetType(CommandHelper.GetClassName(type)).GetMethod("Handle").Invoke(null, new object[] { twitchBot, chatMessage });
+                                    Type.GetType(CommandHelper.GetCommandClassName(type)).GetMethod("Handle").Invoke(null, new object[] { twitchBot, chatMessage });
                                 }
                             });
                         }
