53fdf30d3e1f605ab4daf5fe94f1c5b3e135682a
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 15:24:14 +0200

    added function to ident cmd, not knowingly works

--- OkayegTeaTimeCSharp/Commands/CommandHandler.cs
+++ OkayegTeaTimeCSharp/Commands/CommandHandler.cs
@@ -3,6 +3,10 @@ using OkayegTeaTimeCSharp.Twitch.Bot;
 using System;
 using System.Linq;
 using TwitchLib.Client.Models;
+using OkayegTeaTimeCSharp.Prefixes;
+using OkayegTeaTimeCSharp.Twitch;
+using OkayegTeaTimeCSharp.Utils;
+using OkayegTeaTimeCSharp.Messages;
 
 namespace OkayegTeaTimeCSharp.Commands
 {
@@ -10,14 +14,39 @@ namespace OkayegTeaTimeCSharp.Commands
     {
         public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage)
         {
-#warning not sure if this is the best method to do it
-            ((CommandType[])Enum.GetValues(typeof(CommandType))).ToList().ForEach(type =>
+            if (MessageHelper.IsCommand(chatMessage.GetMessage()))
             {
-                if (CommandHelper.MatchesAlias(chatMessage, type))
+                ((CommandType[])Enum.GetValues(typeof(CommandType))).ToList().ForEach(type =>
                 {
-                    Type.GetType(CommandHelper.GetClassName(type)).GetMethod("Handle").Invoke(null, null);
-                }
-            });
+                    if (CommandHelper.MatchesAlias(chatMessage, type))
+                    {
+                        if (string.IsNullOrEmpty(PrefixHelper.GetPrefix(chatMessage.Channel)))
+                        {
+                            CommandHelper.GetCommand(type).Alias.ForEach(alias =>
+                            {
+                                if (chatMessage.GetMessage().IsMatch(PatternCreator.Create(alias, PrefixType.None)))
+                                {
+                                    Type.GetType(CommandHelper.GetClassName(type)).GetMethod("Handle").Invoke(null, new object[] { twitchBot, chatMessage });
+                                }
+                            });
+                        }
+                        else
+                        {
+                            CommandHelper.GetCommand(type).Alias.ForEach(alias =>
+                            {
+                                if (chatMessage.GetMessage().IsMatch(PatternCreator.Create(alias, PrefixType.Active)))
+                                {
+                                    Type.GetType(CommandHelper.GetClassName(type)).GetMethod("Handle").Invoke(null, new object[] { twitchBot, chatMessage });
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+            else if (MessageHelper.IsAfkCommand(chatMessage.GetMessage()))
+            {
+
+            }
         }
     }
 }
