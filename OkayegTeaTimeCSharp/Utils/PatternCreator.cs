dacc3b37522f842ba79e2ff5ba1be1e58f64843d
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 15:25:16 +0200

    added a pattern creator for cmds

---
+++ OkayegTeaTimeCSharp/Utils/PatternCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OkayegTeaTimeCSharp.Commands.CommandEnums;
+using OkayegTeaTimeCSharp.Commands;
+
+namespace OkayegTeaTimeCSharp.Utils
+{
+    public static class PatternCreator
+    {
+        public static string Create(string alias, PrefixType prefixType, string addition = "")
+        {
+            if (prefixType.Equals(PrefixType.None))
+            {
+                return @"^" + alias + @"eg" + addition;
+            }
+            else //if (prefixType.Equals(PrefixType.Active))
+            {
+                return @"^\S+" + alias + addition;
+            }
+        }
+    }
+}
