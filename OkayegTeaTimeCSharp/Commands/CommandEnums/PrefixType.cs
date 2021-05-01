23430b8bdc03f3d8f99cc82b3402dd94dd9e6796
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 15:25:04 +0200

    added prefix type enum

---
+++ OkayegTeaTimeCSharp/Commands/CommandEnums/PrefixType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandEnums
+{
+    public enum PrefixType
+    {
+        None,
+        Active
+    }
+}
