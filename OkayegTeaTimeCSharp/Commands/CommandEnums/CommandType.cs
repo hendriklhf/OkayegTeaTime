d05e3492a4d55e1752325047e308075c00c7571e
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 02:19:28 +0200

    added enum for command types

---
+++ OkayegTeaTimeCSharp/Commands/CommandEnums/CommandType.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandEnums
+{
+    public enum CommandType
+    {
+        Afk,
+        Chatters,
+        Check,
+        Coinflip,
+        Cookie,
+        Count,
+        Cytube,
+        Emote,
+        First,
+        Fuck,
+        Gachi,
+        Help,
+        Last,
+        Massping,
+        Math,
+        Nuke,
+        Ping,
+        Quote,
+        Rafk,
+        Rand,
+        Remind,
+        Search,
+        Set,
+        Spotify,
+        Suggest,
+        Tuck,
+        Unset,
+        Yourmom
+    }
+    public enum AfkCommandType
+    {
+        Afk,
+        Brb,
+        Fat,
+        Game,
+        Gn,
+        Lurk,
+        Movie,
+        Nap,
+        Poof,
+        School,
+        Shower,
+        Study,
+        Work
+    }
+}
