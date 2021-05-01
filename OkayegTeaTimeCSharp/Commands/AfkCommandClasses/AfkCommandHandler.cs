40064ebb8e3e9ccf885f325ae39ed194df42aa53
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 21:43:21 +0200

    added afkcommandhandler

---
+++ OkayegTeaTimeCSharp/Commands/AfkCommandClasses/AfkCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Commands.AfkCommandClasses
+{
+    public static class AfkCommandHandler
+    {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage)
+        {
+
+        }
+    }
+}
