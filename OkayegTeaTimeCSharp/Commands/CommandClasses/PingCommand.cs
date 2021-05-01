095f4b3e116c0d03d0d9ee3111b0bcb7c6032a10
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-01 15:24:39 +0200

    added ping command function

--- OkayegTeaTimeCSharp/Commands/CommandClasses/PingCommand.cs
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/PingCommand.cs
@@ -1,6 +1,17 @@
-﻿namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using TwitchLib.Client.Models;
+using OkayegTeaTimeCSharp.Prefixes;
+using OkayegTeaTimeCSharp.Twitch;
+using OkayegTeaTimeCSharp.Utils;
+using OkayegTeaTimeCSharp.Commands.CommandEnums;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
 {
     public static class PingCommand
     {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage)
+        {
+            twitchBot.Send(chatMessage.Channel, $"Pongeg, I'm here! Uptime: {twitchBot.Runtime}");
+        }
     }
 }
