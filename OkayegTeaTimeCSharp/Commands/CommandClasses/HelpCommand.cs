f026389e922401611c4f259e62266e41e7e2dda1
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-02 01:38:42 +0200

    help command finished

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/HelpCommand.cs
@@ -0,0 +1,20 @@
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+using OkayegTeaTimeCSharp.Properties;
+using OkayegTeaTimeCSharp.Utils;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public class HelpCommand
+    {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage)
+        {
+            twitchBot.Send(chatMessage.Channel, $"{Emoji.PointRight} {chatMessage.Username}, here you can find a list of commands and the repository: {Resources.GitHubRepoLink}");
+        }
+    }
+}
