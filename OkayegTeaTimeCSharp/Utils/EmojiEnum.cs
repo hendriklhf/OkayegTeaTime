ef8db71e2b8a8e7e14c59dd15d806737f0318aef
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-02 01:38:25 +0200

    added emoji enum, ToString method missing

---
+++ OkayegTeaTimeCSharp/Utils/EmojiEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkayegTeaTimeCSharp.Utils
+{
+    public enum Emoji
+    {
+        [Description("👉")]
+        PointRight
+    }
+}
