ec86e8c94a868fae7296f1c029e6386f106cba95
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-03 00:00:00 +0200

    quote cmd class added

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/QuoteCommand.cs
@@ -0,0 +1,16 @@
+﻿using OkayegTeaTimeCSharp.Commands.CommandEnums;
+using OkayegTeaTimeCSharp.Twitch.Bot;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public static class QuoteCommand
+    {
+        public const CommandType Type = CommandType.Quote;
+
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
+        {
+#warning not implemented
+        }
+    }
+}
