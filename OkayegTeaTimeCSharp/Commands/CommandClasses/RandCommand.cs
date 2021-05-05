d06188f43b91314a6c2be265a43cebe93fb8c2b2
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-05 12:14:37 +0200

    rand cmd added

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/RandCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OkayegTeaTimeCSharp.Commands.CommandEnums;
+using OkayegTeaTimeCSharp.Twitch.Bot;
+using OkayegTeaTimeCSharp.Utils;
+using TwitchLib.Client.Models;
+using OkayegTeaTimeCSharp.Twitch;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public static class RandCommand
+    {
+        public const CommandType Type = CommandType.Rand;
+
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
+        {
+            if (chatMessage.GetMessage().IsMatch(PatternCreator.CreateBoth(alias, @"\s\w+\s#?\w+$")))
+            {
+                twitchBot.SendRandomMessage(chatMessage, chatMessage.GetLowerSplit()[1], chatMessage.GetLowerSplit()[2].Replace("#", ""));
+            }
+            else if (chatMessage.GetMessage().IsMatch(PatternCreator.CreateBoth(alias, @"\s\w+$")))
+            {
+                twitchBot.SendRandomMessage(chatMessage, chatMessage.GetLowerSplit()[1]);
+            }
+            else if (chatMessage.GetMessage().IsMatch(PatternCreator.CreateBoth(alias, @"$")))
+            {
+                twitchBot.SendRandomMessage(chatMessage);
+            }
+        }
+    }
+}
