4ef27441ce9c79f2ec9b3381761c2cc393efb822
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-14 22:10:10 +0200

    ctor fix

--- OkayegTeaTimeCSharp/Database/Models/Suggestion.cs
+++ OkayegTeaTimeCSharp/Database/Models/Suggestion.cs
@@ -13,6 +13,16 @@ namespace OkayegTeaTimeCSharp.Database.Models
         public long Time { get; set; } = TimeHelper.Now();
         public bool? Done { get; set; } = false;
 
+        public Suggestion(int id, string username, byte[] suggestion1, string channel, long time, bool? done)
+        {
+            Id = id;
+            Username = username;
+            Suggestion1 = suggestion1;
+            Channel = channel;
+            Time = time;
+            Done = done;
+        }
+
         public Suggestion(string username, byte[] suggestion, string channel)
         {
             Username = username;
