ef330bcd329b980a13e364d843360c63e002e991
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-15 02:58:29 +0200

    ctors created

--- OkayegTeaTimeCSharp/Database/Models/Prefix.cs
+++ OkayegTeaTimeCSharp/Database/Models/Prefix.cs
@@ -7,5 +7,18 @@ namespace OkayegTeaTimeCSharp.Database.Models
         public int Id { get; set; }
         public string Channel { get; set; }
         public string PrefixString { get; set; }
+
+        public Prefix(int id, string channel, string prefixString)
+        {
+            Id = id;
+            Channel = channel;
+            PrefixString = prefixString;
+        }
+
+        public Prefix(string channel, string prefixString)
+        {
+            Channel = $"#{channel.Replace("#", "")}";
+            PrefixString = prefixString;
+        }
     }
 }
