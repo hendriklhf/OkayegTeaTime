122a7b1f01131b03075e3f182db41fb5ed31cb90
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-16 04:23:09 +0200

    ctors created

--- OkayegTeaTimeCSharp/Database/Models/Reminder.cs
+++ OkayegTeaTimeCSharp/Database/Models/Reminder.cs
@@ -1,5 +1,7 @@
 ﻿#nullable disable
 
+using OkayegTeaTimeCSharp.Time;
+
 namespace OkayegTeaTimeCSharp.Database.Models
 {
     public partial class Reminder
@@ -9,7 +11,31 @@ namespace OkayegTeaTimeCSharp.Database.Models
         public string ToUser { get; set; }
         public byte[] Message { get; set; }
         public string Channel { get; set; }
-        public long Time { get; set; }
-        public long ToTime { get; set; }
+        public long Time { get; set; } = TimeHelper.Now();
+        public long ToTime { get; set; } = 0;
+
+        public Reminder(int id, string fromUser, string toUser, byte[] message, string channel, long time, long toTime)
+        {
+            Id = id;
+            FromUser = fromUser;
+            ToUser = toUser;
+            Message = message;
+            Channel = channel;
+            Time = time;
+            ToTime = toTime;
+        }
+
+        public Reminder(string fromUser, string toUser, byte[] message, string channel)
+        {
+            FromUser = fromUser;
+            ToUser = toUser;
+            Message = message;
+            Channel = channel;
+        }
+
+        public Reminder(string fromUser, string toUser, byte[] message, string channel, long time, long toTime) : this(fromUser, toUser, message, channel)
+        {
+            ToTime = toTime;
+        }
     }
 }
