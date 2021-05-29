7dff7a28e0597df734b6feef5c17beeee54bcf87
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-05-29 17:43:18 +0200

    smol beginning of spotify cmd

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/SpotifyCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OkayegTeaTimeCSharp.Twitch;
+using OkayegTeaTimeCSharp.Twitch.Bot;
+using OkayegTeaTimeCSharp.Utils;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public static class SpotifyCommand
+    {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
+        {
+
+        }
+    }
+}
---
+++ OkayegTeaTimeCSharp/Spotify/SpotifyClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAPI;
+using SpotifyAPI.Web;
+using SpotifyAPI = SpotifyAPI.Web;
+using SpotifyAPI.Web.Http;
+using OkayegTeaTimeCSharp.Properties;
+
+namespace OkayegTeaTimeCSharp.Spotify
+{
+    public class SpotifyRequest
+    {
+        public async static Task GetNewAuthToken(string code)
+        {
+            var response = await new OAuthClient().RequestToken(new AuthorizationCodeTokenRequest(Resources.SpotifyClientID, Resources.SpotifyClientSecret, code, new Uri("example.com/callback")));
+        }
+
+        public static void GetCurrentlyPlaying(string accesToken)
+        {
+            Console.WriteLine(new SpotifyClient(accesToken).Player.GetCurrentlyPlaying(new PlayerCurrentlyPlayingRequest()).Result.Context.Type);
+        }
+    }
+}
