ba877e06529e6b98ab064ed279710177abc79946
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-02 00:52:38 +0200

    created emote class

---
+++ OkayegTeaTimeCSharp/HttpRequests/Emote.cs
@@ -0,0 +1,20 @@
+﻿namespace OkayegTeaTimeCSharp.HttpRequests
+{
+    public class Emote
+    {
+        public int Index { get; }
+
+        public string Name { get; }
+
+        public Emote(int index, string name)
+        {
+            Index = index;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
