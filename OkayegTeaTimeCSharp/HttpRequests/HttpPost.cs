6feb582014184cafe78cf6f408cbe22a34fca8ee
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-02 01:20:44 +0200

    created HttpPost class

---
+++ OkayegTeaTimeCSharp/HttpRequests/HttpPost.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OkayegTeaTimeCSharp.HttpRequests
+{
+    public class HttpPost
+    {
+        public string URL { get; }
+
+        public JsonElement Data { get; private set; }
+
+        private readonly HttpClient _httpClient = new();
+
+        public HttpPost(string url)
+        {
+            URL = url;
+            Data = JsonSerializer.Deserialize<JsonElement>(PostRequest().Result);
+        }
+
+        private async Task<string> PostRequest()
+        {
+            return "";
+        }
+    }
+}
