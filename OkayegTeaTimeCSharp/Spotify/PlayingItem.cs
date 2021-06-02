73cc5c842476af7f3cb39f3a29ffb2392e7d49da
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-02 15:08:16 +0200

    created playingitem class

---
+++ OkayegTeaTimeCSharp/Spotify/PlayingItem.cs
@@ -0,0 +1,21 @@
+﻿using SpotifyAPI.Web;
+
+namespace OkayegTeaTimeCSharp.Spotify
+{
+    public class PlayingItem
+    {
+        public string Title { get; }
+
+        public string Artist { get; }
+
+        public string Message { get; private set; }
+
+        public ItemType ItemType { get; }
+
+        private readonly IPlayableItem _item;
+
+        public PlayingItem(CurrentlyPlaying currentlyPlaying)
+        {
+        }
+    }
+}
