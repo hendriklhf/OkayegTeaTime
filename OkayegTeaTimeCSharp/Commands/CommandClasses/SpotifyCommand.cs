c5793b2e6332afa864a0e3d4d610665d231d9cd6
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-03 15:56:58 +0200

    spotify cmd class finished

--- OkayegTeaTimeCSharp/Commands/CommandClasses/SpotifyCommand.cs
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/SpotifyCommand.cs
@@ -1,4 +1,6 @@
-﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+﻿using OkayegTeaTimeCSharp.Twitch;
+using OkayegTeaTimeCSharp.Twitch.Bot;
+using OkayegTeaTimeCSharp.Utils;
 using TwitchLib.Client.Models;
 
 namespace OkayegTeaTimeCSharp.Commands.CommandClasses
@@ -7,6 +9,14 @@ namespace OkayegTeaTimeCSharp.Commands.CommandClasses
     {
         public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
         {
+            if (chatMessage.GetMessage().IsMatch(PatternCreator.CreateBoth(alias, @"\s\w+")))
+            {
+                twitchBot.SendSpotifyCurrentlyPlaying(chatMessage);
+            }
+            else if (chatMessage.GetMessage().IsMatch(PatternCreator.CreateBoth(alias)))
+            {
+                twitchBot.SendSpotifyCurrentlyPlaying(chatMessage);
+            }
         }
     }
 }
