810ded242d701a18c58f834fe523b71bf9d3f548
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-03 15:56:45 +0200

    emote cmd class created

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/EmoteCommand.cs
@@ -0,0 +1,12 @@
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public class EmoteCommand
+    {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
+        {
+        }
+    }
+}
