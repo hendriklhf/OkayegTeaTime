72dc5878bb3415bb3aae7062da19ca619f6e2e5d
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-03 14:12:25 +0200

    ctors for spotify database added

--- OkayegTeaTimeCSharp/Database/Models/Spotify.cs
+++ OkayegTeaTimeCSharp/Database/Models/Spotify.cs
@@ -1,5 +1,7 @@
 ﻿#nullable disable
 
+using OkayegTeaTimeCSharp.Time;
+
 namespace OkayegTeaTimeCSharp.Database.Models
 {
     public partial class Spotify
@@ -8,6 +10,22 @@ namespace OkayegTeaTimeCSharp.Database.Models
         public string Username { get; set; }
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
-        public long Time { get; set; }
+        public long Time { get; set; } = TimeHelper.Now();
+
+        public Spotify(string username, string accessToken, string refreshToken)
+        {
+            Username = username;
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+
+        public Spotify(int id, string username, string accessToken, string refreshToken, long time)
+        {
+            Id = id;
+            Username = username;
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+            Time = time;
+        }
     }
 }
