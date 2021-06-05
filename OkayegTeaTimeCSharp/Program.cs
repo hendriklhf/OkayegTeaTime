317f4d46ddca711fc0e8e68ccc1218d5ff1b5fd3
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-05 04:12:48 +0200

    edited notes

--- OkayegTeaTimeCSharp/Program.cs
+++ OkayegTeaTimeCSharp/Program.cs
@@ -35,7 +35,7 @@ namespace OkayegTeaTimeCSharp
 #warning poopeg, pisseg
 #warning code needs doc
 #warning move databasehelper methods
-#warning nukes wont be deleted
 #warning "randeg strbhlfe" always sends my first message
 #warning remove "(no message)" from afk messages
 #warning twitch api access token needs to be renewed after 60days
+#warning cookieg always shows the same cookie
