3096da89983562695dee3ba5432b6cb43327b0a3
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-05 04:15:40 +0200

    fixed pattern

--- OkayegTeaTimeCSharp/Utils/Pattern.cs
+++ OkayegTeaTimeCSharp/Utils/Pattern.cs
@@ -3,6 +3,6 @@
     public class Pattern
     {
         public const string ReminderInTimePattern = @"\s\w+\sin\s(" + TimeSplitPattern + @"\s)+(\S|\s)+";
-        public const string TimeSplitPattern = @"(\d+(y(ear)?|d(ay)?|h(our)?|m(in(ute)?)?|s(ecs(ond)?)?)s?)";
+        public const string TimeSplitPattern = @"(\d+(y(ear)?|d(ay)?|h(our)?|m(in(ute)?)?|s(ec(ond)?)?)s?)";
     }
 }
