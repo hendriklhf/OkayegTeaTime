5c87be3c20f6ac3f875e868fc03634b5cc5a04c2
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-08 08:38:49 +0200

    fuck cmd file created

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/FuckCommand.cs
@@ -0,0 +1,13 @@
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public static class FuckCommand
+    {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
+        {
+#warning needs implementation
+        }
+    }
+}
