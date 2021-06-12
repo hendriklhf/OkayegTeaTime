d206a3cf3bbd7052908e75f6377c6ae1e7f4778f
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-12 21:00:57 +0200

    changed pattern to make reminder without messages possible

--- OkayegTeaTimeCSharp/Utils/Pattern.cs
+++ OkayegTeaTimeCSharp/Utils/Pattern.cs
@@ -2,7 +2,7 @@
 {
     public class Pattern
     {
-        public const string ReminderInTimePattern = @"\s\w+\sin\s(" + TimeSplitPattern + @"\s)+(\S|\s)+";
+        public const string ReminderInTimePattern = @"\s\w+\sin\s(" + TimeSplitPattern + @"(\s|$))+(\S+|\s)*";
         public const string TimeSplitPattern = @"(\d+(y(ear)?|d(ay)?|h(our)?|m(in(ute)?)?|s(ec(ond)?)?)s?)";
     }
 }
