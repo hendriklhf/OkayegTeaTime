c5ffecf69c0b9e758b00ccaa8643ef6ae1d0cd0d
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-15 00:41:41 +0200

    added doc to dotted number

--- OkayegTeaTimeCSharp/Utils/DottedNumber.cs
+++ OkayegTeaTimeCSharp/Utils/DottedNumber.cs
@@ -6,10 +6,20 @@
     /// </summary>
     public struct DottedNumber
     {
+        /// <summary>
+        /// The number with the dots inserted.
+        /// </summary>
         public string Number { get; }
 
+        /// <summary>
+        /// The original number passed to the constructor.
+        /// </summary>
         public long OrigninalNumber { get; }
 
+        /// <summary>
+        /// The basic constructor of DottedNumber.
+        /// </summary>
+        /// <param name="number">A number of type long in which the dots will be inserted</param>
         public DottedNumber(long number)
         {
             string num = number.ToString();
@@ -24,9 +34,10 @@
             OrigninalNumber = number;
         }
 
-        public override string ToString()
-        {
-            return Number;
-        }
+        /// <summary>
+        /// Override method of ToString() that returns the result.
+        /// </summary>
+        /// <returns>the result dotted number</returns>
+        public override string ToString() => Number;
     }
 }
