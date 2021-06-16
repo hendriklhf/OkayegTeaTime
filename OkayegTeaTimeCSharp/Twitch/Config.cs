d0f89facdca41132832247a44e91b45c1fedb897
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-16 19:07:32 +0200

    added method to get not allowed to write in channels

--- OkayegTeaTimeCSharp/Twitch/Config.cs
+++ OkayegTeaTimeCSharp/Twitch/Config.cs
@@ -9,12 +9,17 @@ namespace OkayegTeaTimeCSharp.Twitch
     {
         public const int AfkCooldown = 10000;
         public const string EmoteInFront = "Okayeg";
-        public const string Suffix = "eg";
         public const int MaxMessageLength = 500;
+        public const string Suffix = "eg";
 
         public static List<string> GetChannels()
         {
             return new OkayegTeaTimeContext().Bots.Where(b => b.Id == 1).FirstOrDefault().Channels.Split().ToList();
+        }
+
+        public static List<string> GetNotAllowedChannels()
+        {
+            return Resources.NotAllowedChannels.Split().ToList();
         }
 
         public static List<string> GetNotLoggedChannels()
