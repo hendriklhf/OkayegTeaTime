dcf791374de8d7a9a7860d52cc045b23993a5564
Sterbehilfe <hendrik.leerhoff28@gmail.com>
2021-06-16 19:03:29 +0200

    removed tasks

--- OkayegTeaTimeCSharp/Program.cs
+++ OkayegTeaTimeCSharp/Program.cs
@@ -35,12 +35,9 @@ namespace OkayegTeaTimeCSharp
     }
 }
 
-#warning needs a list of channels in which the bot is not allowed to send messages
 #warning change prefixstring column to varbinary to support emojis
 #warning poopeg, pisseg
 #warning code needs doc
 #warning move databasehelper methods
-#warning "randeg strbhlfe" always sends my first message
-#warning cookieg always shows the same cookie
 #warning emote cmd not available for external channels
 #warning create own class library
