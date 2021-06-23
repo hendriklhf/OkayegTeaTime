e415696057e090a0260ef82c05d9d87115c41420
Sterbehilfe <twitch.tv/strbhlfe>
2021-06-23 16:44:13 +0200

    removed notes

--- OkayegTeaTimeCSharp/Program.cs
+++ OkayegTeaTimeCSharp/Program.cs
@@ -37,9 +37,7 @@ namespace OkayegTeaTimeCSharp
     }
 }
 
-#warning change prefixstring column to varbinary to support emojis
 #warning code needs doc
 #warning move databasehelper methods
 #warning emote cmd not available for external channels
-#warning limit the amount of reminders for one person
 #warning discord
