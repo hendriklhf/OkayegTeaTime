90cc1d7bc155b53d0ba192a8feaf79e18879d2da
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-04 19:15:18 +0200

    added ps scripts to build for win64 and linux-arm

---
+++ OkayegTeaTimeCSharp/Tools/Builder/linux-arm.ps1
@@ -0,0 +1,4 @@
+﻿dotnet publish -o ..\..\bin\Publish\LinuxArm -c Release -r linux-arm -p:PublishSingleFile=true --self-contained true ..\..\OkayegTeaTimeCSharp.csproj
+xcopy ..\..\Resources ..\..\bin\Publish\LinuxArm\Resources /E /Y /I
+xcopy ..\..\JsonData\Commands.json ..\..\bin\Publish\LinuxArm\Resources /E /Y /I
+Remove-Item ..\..\bin\Publish\LinuxArm\Resources\JsonClasses -Recurse -Force -Confirm:$false
---
+++ OkayegTeaTimeCSharp/Tools/Builder/win64.ps1
@@ -0,0 +1,4 @@
+﻿dotnet publish -o ..\..\bin\Publish\Win-x64 -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true ..\..\OkayegTeaTimeCSharp.csproj
+xcopy ..\..\Resources ..\..\bin\Publish\Win-x64\Resources /E /Y /I
+xcopy ..\..\JsonData\Commands.json ..\..\bin\Publish\Win-x64\Resources /E /Y /I
+Remove-Item ..\..\bin\Publish\Win-x64\Resources\JsonClasses -Recurse -Force -Confirm:$false
