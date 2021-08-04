35781e31e5c3af0d7b0fee3da6490a359ffdbc08
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-04 19:29:29 +0200

    Commands.json moved to Resources folder

--- OkayegTeaTimeCSharp/Tools/Builder/win64.ps1
+++ OkayegTeaTimeCSharp/Tools/Builder/win64.ps1
@@ -1,4 +1,2 @@
 ﻿dotnet publish -o ..\..\bin\Publish\Win-x64 -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true ..\..\OkayegTeaTimeCSharp.csproj
 xcopy ..\..\Resources ..\..\bin\Publish\Win-x64\Resources /E /Y /I
-xcopy ..\..\JsonData\Commands.json ..\..\bin\Publish\Win-x64\Resources /E /Y /I
-Remove-Item ..\..\bin\Publish\Win-x64\Resources\JsonClasses -Recurse -Force -Confirm:$false
