38f2492704294937508187f834855f757348c7cd
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2021-08-09 16:11:17 +0200

    fixed namespace wrapping

--- OkayegTeaTimeCSharp/HttpRequests/Chatter.cs
+++ OkayegTeaTimeCSharp/HttpRequests/Chatter.cs
@@ -1,14 +1,15 @@
 namespace OkayegTeaTimeCSharp.HttpRequests;
-
-public class Chatter
 {
-    public string Username { get; }
-    
-    public ChatRole ChatRole { get; }
-    
-    public Chatter(string username, ChatRole chatRole)
+    public class Chatter
     {
-        Username = username;
-        ChatRole = charRole;
+        public string Username { get; }
+
+        public ChatRole ChatRole { get; }
+
+        public Chatter(string username, ChatRole chatRole)
+        {
+            Username = username;
+            ChatRole = charRole;
+        }
     }
 }
