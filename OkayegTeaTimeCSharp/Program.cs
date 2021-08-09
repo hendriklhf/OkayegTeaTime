985e34a473638f8a38e3102d394f5fd1928bd2c1
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-09 19:47:04 +0200

    fixed typo Okayeg

--- OkayegTeaTimeCSharp/Program.cs
+++ OkayegTeaTimeCSharp/Program.cs
@@ -24,7 +24,7 @@ namespace OkayegTeaTimeCSharp
 #endif
             while (_running)
             {
-                Consolse.ReadLine();
+                Console.ReadLine();
             }
         }
 
