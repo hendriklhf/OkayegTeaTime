4481dac9a898e82a2b0eac201b4c99bcd2e5e329
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-28 23:02:16 +0200

    added more UserLists to json config

--- OkayegTeaTimeCSharp/JsonData/JsonClasses/Data/BotData.cs
+++ OkayegTeaTimeCSharp/JsonData/JsonClasses/Data/BotData.cs
@@ -3,9 +3,5 @@ namespace OkayegTeaTimeCSharp.JsonData.JsonClasses.Data
     public class BotData
     {
         public UserLists UserLists { get; set; }
-
-        public EgData EgData { get; set; }
-
-        public Discord Discord { get; set; }
     }
 }
--- OkayegTeaTimeCSharp/JsonData/JsonClasses/Data/UserLists.cs
+++ OkayegTeaTimeCSharp/JsonData/JsonClasses/Data/UserLists.cs
@@ -4,6 +4,10 @@ namespace OkayegTeaTimeCSharp.JsonData.JsonClasses.Data
 {
     public class UserLists
     {
+        public List<string> Owners { get; set; }
+
+        public List<string> Moderators { get; set; }
+
         public List<string> SpecialUsers { get; set; }
 
         public List<string> SecretUsers { get; set; }
