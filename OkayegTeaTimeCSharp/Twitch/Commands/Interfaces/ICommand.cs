a135f427ad7889c55d4c5e59bfb9ea98ea129fd6
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-28 12:25:31 +0200

    extended command interface

--- OkayegTeaTimeCSharp/Twitch/Commands/Interfaces/ICommand.cs
+++ OkayegTeaTimeCSharp/Twitch/Commands/Interfaces/ICommand.cs
@@ -1,7 +1,16 @@
-﻿namespace OkayegTeaTimeCSharp.Twitch.Commands.Interfaces
+﻿using OkayegTeaTimeCSharp.Twitch.Bot;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Twitch.Commands.Interfaces
 {
     public interface ICommand
     {
+        public TwitchBot TwitchBot { get; }
+
+        public ChatMessage ChatMessage { get; }
+
+        public string Alias { get; }
+
         public void Handle();
     }
 }
