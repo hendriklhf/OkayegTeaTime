6fd378eb744b39dcbf10f2aaeecadf857d6a4e0f
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-28 14:48:42 +0200

    fixed default IsAfk value

--- OkayegTeaTimeCSharp/Database/Models/User.cs
+++ OkayegTeaTimeCSharp/Database/Models/User.cs
@@ -9,7 +9,7 @@ namespace OkayegTeaTimeCSharp.Database.Models
         public byte[] MessageText { get; set; }
         public string Type { get; set; }
         public long Time { get; set; } = 0;
-        public string IsAfk { get; set; } = "false";
+        public string IsAfk { get; set; } = "False";
 
         public User(int id, string username, byte[] messageText, string type, long time, string isAfk)
         {
