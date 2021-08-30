f33f2aaa27c5000304860b1a0beb4aeb59d6126e
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-30 20:19:11 +0200

    moved readme generator to a js script

--- OkayegTeaTimeCSharp/Tools/Builder/linux-arm.ps1
+++ OkayegTeaTimeCSharp/Tools/Builder/linux-arm.ps1
@@ -1,2 +1,3 @@
 ﻿dotnet publish -o ..\..\bin\Publish\LinuxArm -c Release -r linux-arm -p:PublishSingleFile=true --self-contained true ..\..\OkayegTeaTimeCSharp.csproj
 xcopy ..\..\Resources ..\..\bin\Publish\LinuxArm\Resources /E /Y /I
+node ..\GitHub\ReadMeGenerator.js
--- OkayegTeaTimeCSharp/Tools/Builder/win64.ps1
+++ OkayegTeaTimeCSharp/Tools/Builder/win64.ps1
@@ -1,2 +1,3 @@
 ﻿dotnet publish -o ..\..\bin\Publish\Win-x64 -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true ..\..\OkayegTeaTimeCSharp.csproj
 xcopy ..\..\Resources ..\..\bin\Publish\Win-x64\Resources /E /Y /I
+node ..\GitHub\ReadMeGenerator.js
