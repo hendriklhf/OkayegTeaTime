bf080245a20a2ab63a59796234c5a35607f3e148
Sterbehilfe <twitch.tv/strbhlfe>
2021-09-03 00:27:40 +0200

    added method to remove "@"

--- OkayegTeaTimeCSharp/Utils/StringHelper.cs
+++ OkayegTeaTimeCSharp/Utils/StringHelper.cs
@@ -12,6 +12,14 @@ namespace OkayegTeaTimeCSharp.Utils
             return str.Remove("#");
         }
 
+        /// <summary>
+        /// Removes the <see cref="char"/> '@' from the <see cref="string"/> <paramref name="str"/>. Doesn't remove something "at" an index.
+        /// </summary>
+        public static string RemoveTheAt(this string str)
+        {
+            return str.Remove("@");
+        }
+
         public static List<string> WordArrayStringToList(this string input)
         {
             return Regex.Matches(input, @"\w+", RegexOptions.IgnoreCase).Select(m => m.Value).ToList();
