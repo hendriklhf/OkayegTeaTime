b0e65745ed2cf27aa02efbbe9e033ba4f60beac6
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2021-10-10 17:01:05 +0200

    added build for macOS

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
-dotnet publish -o .\Build\Win-x64 -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true .\OkayegTeaTimeCSharp\OkayegTeaTimeCSharp.csproj
-dotnet publish -o .\Build\LinuxArm -c Release -r linux-arm -p:PublishSingleFile=true --self-contained true .\OkayegTeaTimeCSharp\OkayegTeaTimeCSharp.csproj
-xcopy .\OkayegTeaTimeCSharp\Resources .\Build\Win-x64\Resources /E /Y /I
-xcopy .\OkayegTeaTimeCSharp\Resources .\Build\LinuxArm\Resources /E /Y /I
+dotnet publish -o .\Build\win-x64 -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true .\OkayegTeaTimeCSharp\OkayegTeaTimeCSharp.csproj
+dotnet publish -o .\Build\linux-arm -c Release -r linux-arm -p:PublishSingleFile=true --self-contained true .\OkayegTeaTimeCSharp\OkayegTeaTimeCSharp.csproj
+dotnet publish -o .\Build\macOS -c Release -r osx-x64 -p:PublishSingleFile=true --self-contained true .\OkayegTeaTimeCSharp\OkayegTeaTimeCSharp.csproj
+xcopy .\OkayegTeaTimeCSharp\Resources .\Build\win-x64\Resources /E /Y /I
+xcopy .\OkayegTeaTimeCSharp\Resources .\Build\linux-arm\Resources /E /Y /I
+xcopy .\OkayegTeaTimeCSharp\Resources .\Build\macOS\Resources /E /Y /I
 
 dotnet test .\Tests\Tests.csproj
 
@@ -13,10 +15,14 @@ cd ..\..
 
 cd .\Starter
 go env -w GOOS=linux GOARCH=arm
-go build -o ..\Build\LinuxArm
-go env -w GOOS=windows GOARCH=amd64
-go build -o ..\Build\Win-x64
-Write-Output "Built Starter"
+go build -o ..\Build\linux-arm
+Write-Output "Built Starter for linux-arm"
+go env -w GOOS=darwin GOARCH=amd64
+go build -o ..\build\macOS
+Write-Output "Built Starter for macOS"
+go env -w GOOS=windows
+go build -o ..\Build\win-x64
+Write-Output "Built Starter for win-x64"
 cd ..
 
 Write-Output "=============="
