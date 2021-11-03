32959aff8cb12814b8caea8b9f23601c5d44b820
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2021-11-03 12:38:33 +0100

    converted connection string to a json file

---
+++ OkayegTeaTimeCSharp/JsonData/JsonClasses/DatabaseConnection/Connection.cs
@@ -0,0 +1,14 @@
+﻿namespace OkayegTeaTimeCSharp.JsonData.JsonClasses.DatabaseConnection;
+
+public class Connection
+{
+    public string Hostname { get; set; }
+
+    public string Database { get; set; }
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public string ConnectionString => $"Data Source={Hostname};DataBase={Database};User ID={Username};Password={Password};";
+}
--- OkayegTeaTimeCSharp/JsonData/JsonController.cs
+++ OkayegTeaTimeCSharp/JsonData/JsonController.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Text.Json;
 using OkayegTeaTimeCSharp.JsonData.JsonClasses.CommandData;
+using OkayegTeaTimeCSharp.JsonData.JsonClasses.DatabaseConnection;
 using OkayegTeaTimeCSharp.JsonData.JsonClasses.Settings;
 using Path = OkayegTeaTimeCSharp.Properties.Path;
 
@@ -12,9 +13,12 @@ public static class JsonController
 
     public static CommandLists CommandLists { get; private set; }
 
+    public static Connection Connection { get; private set; }
+
     public static void LoadData()
     {
         Settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(Path.SettingsJson));
         CommandLists = JsonSerializer.Deserialize<CommandLists>(File.ReadAllText(Path.CommandsJson));
+        Connection = JsonSerializer.Deserialize<Connection>(File.ReadAllText(Path.ConnectionString));
     }
 }
