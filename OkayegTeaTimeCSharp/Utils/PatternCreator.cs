39ab0a51aa1f438a59b66054d042370217bbde00
James Upjohn <jammehcow@jammehcow.co.nz>
2021-12-05 19:47:17 +1300

    feat(command): compile command regex on first invocation

--- OkayegTeaTimeCSharp/Utils/PatternCreator.cs
+++ OkayegTeaTimeCSharp/Utils/PatternCreator.cs
@@ -1,11 +1,26 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
 
 namespace OkayegTeaTimeCSharp.Utils;
 
 public static class PatternCreator
 {
-    public static string Create(string alias, string prefix, string addition = "")
+    private static readonly IDictionary<string, Regex> _cachedPatterns = new ConcurrentDictionary<string, Regex>();
+
+    public static Regex Create(string alias, string prefix, string addition = "")
     {
-        return string.IsNullOrEmpty(prefix) ? "^" + Regex.Escape(alias + Config.Suffix) + addition : "^" + Regex.Escape(prefix) + Regex.Escape(alias) + addition;
+        // TODO: kinda don't like how this is passed in
+        var patternPrefix = string.IsNullOrEmpty(prefix)
+            ? "^" + Regex.Escape(alias + Config.Suffix) + addition
+            : "^" + Regex.Escape(prefix + alias) + addition;
+
+        var patternKey = patternPrefix + addition;
+
+        var cachedPattern = _cachedPatterns[patternKey];
+        if (cachedPattern is not null)
+            return cachedPattern;
+
+        _cachedPatterns.Add(patternKey, new Regex(patternKey, RegexOptions.Compiled | RegexOptions.Singleline));
+        return _cachedPatterns[patternKey];
     }
 }
