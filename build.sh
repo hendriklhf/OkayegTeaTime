0e5e738a47cb830c39be69d8f77cc7349db0272e
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-01-08 01:21:10 +0100

    added readmegenerator and sqlcreateformatter to build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ if [[ "$OSTYPE" == "msys" ]]; then
 fi
 
 go build -o ./publisher${ending} ./Tools/Publisher/main.go
+
+cd ./Tools/ReadMeGenerator/
+go build -o ../../readmegenerator${ending} ./main.go
+cd ../SqlCreateFormatter/
+go build -o ../../formatter${ending} ./main.go
+cd ../..
