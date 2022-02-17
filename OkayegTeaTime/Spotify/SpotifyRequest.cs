877d9999617f39f7ec87bd567dc2441bcdd8602a
benASTRO <81444451+benASTRO@users.noreply.github.com>
2022-02-17 19:23:08 +0100

    Update SpotifyRequest.cs

--- OkayegTeaTime/Spotify/SpotifyRequest.cs
+++ OkayegTeaTime/Spotify/SpotifyRequest.cs
@@ -80,13 +80,14 @@ public static class SpotifyRequest
 
     public static async Task<PlayingItem?> GetCurrentlyPlayingTrack(string username)
     {
+        if (user is null)
+        {
+            return null;
+        }
+        
         SpotifyUser? user = await GetSpotifyUser(username);
         CurrentlyPlaying response = await new SpotifyClient(user.AccessToken).Player.GetCurrentlyPlaying(new());
         PlayingItem? item = SpotifyHelper.GetPlayingItem(response);
-        if (user is null)
-        {
-            return item;
-        }
         
         if (item is not null)
         {
