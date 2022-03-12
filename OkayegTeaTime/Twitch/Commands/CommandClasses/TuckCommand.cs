a5504f5d77036a39cf926b7de35b9c0b62d0ffa6
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-03-12 16:25:07 +0100

    DANKMAN

--- OkayegTeaTime/Twitch/Commands/CommandClasses/TuckCommand.cs
+++ OkayegTeaTime/Twitch/Commands/CommandClasses/TuckCommand.cs
@@ -15,11 +15,15 @@ public class TuckCommand : Command
     public override void Handle()
     {
         Regex pattern = PatternCreator.Create(Alias, ChatMessage.Channel.Prefix, @"\s\w+(\s\S+)?");
-        if (!pattern.IsMatch(ChatMessage.Message))
+        if (pattern.IsMatch(ChatMessage.Message))
         {
             string target = ChatMessage.LowerSplit[1];
+            Response = $"{Emoji.PointRight} {Emoji.Bed} {ChatMessage.Username} tucked {target} to bed";
             string emote = ChatMessage.LowerSplit.Length > 2 ? ChatMessage.Split[2] : string.Empty;
-            Response = $"{Emoji.PointRight} {Emoji.Bed} {ChatMessage.Username} tucked {target} to bed {emote}".Trim();
+            if (!string.IsNullOrEmpty(emote))
+            {
+                Response += $" {emote}";
+            }
         }
     }
 }
