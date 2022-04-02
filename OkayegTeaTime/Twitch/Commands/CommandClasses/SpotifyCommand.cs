b1ae73f912aac99f50d07c19943a2eeb23838592
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-04-02 16:46:10 +0200

    catching exception and rearranged some code

--- OkayegTeaTime/Twitch/Commands/CommandClasses/SpotifyCommand.cs
+++ OkayegTeaTime/Twitch/Commands/CommandClasses/SpotifyCommand.cs
@@ -2,6 +2,7 @@
 using OkayegTeaTime.Database;
 using OkayegTeaTime.Database.Models;
 using OkayegTeaTime.Spotify;
+using OkayegTeaTime.Spotify.Exceptions;
 using OkayegTeaTime.Twitch.Bot;
 using OkayegTeaTime.Twitch.Models;
 
@@ -21,50 +22,54 @@ public class SpotifyCommand : Command
                 ? ChatMessage.Username
                 : ChatMessage.LowerSplit[1]
             : ChatMessage.Channel;
-        Response = $"{ChatMessage.Username}, ";
         bool targetIsSender = username == ChatMessage.Username;
         Task.Run(async () =>
         {
             SpotifyUser? user = DbControl.SpotifyUsers[username];
             if (user is null)
             {
-                if (targetIsSender)
-                {
-                    Response += "can't get your currently playing song, you have to register first";
-                }
-                else
-                {
-                    Response += $"can't get the currently playing song of {username.Antiping()}, they have to register first";
-                }
+                Response = targetIsSender
+                    ? $"{ChatMessage.Username}, can't get your currently playing song, you have to register first"
+                    : $"{ChatMessage.Username}, can't get the currently playing song of {username.Antiping()}, they have to register first";
                 return;
             }
 
-            SpotifyItem? item = await user.GetCurrentlyPlayingItem();
-            if (item is null)
+            SpotifyItem? item;
+            try
             {
-                if (targetIsSender)
-                {
-                    Response += "you aren't listening to anything";
-                }
-                else
-                {
-                    Response += $"{username.Antiping()} is not listening to anything";
-                }
+                item = await user.GetCurrentlyPlayingItem();
+            }
+            catch (SpotifyException ex)
+            {
+                Response = $"{ChatMessage.Username}, {ex.Message}";
                 return;
             }
 
-            if (item is SpotifyTrack track)
+            switch (item)
             {
-                string artists = string.Join(", ", track.Artists.Select(a => a.Name));
-                Response += $"{track.Name} by {artists} || {(track.IsLocal ? "local file" : track.Uri)}";
-            }
-            else if (item is SpotifyEpisode episode)
-            {
-                Response += $"{episode.Name} by {episode.Show.Name} || {(episode.IsLocal ? "local file" : episode.Uri)}";
-            }
-            else
-            {
-                Response += "listening to an unknown Spotify item type monkaS";
+                case null:
+                {
+                    Response = targetIsSender
+                        ? $"{ChatMessage.Username}, you aren't listening to anything"
+                        : $"{ChatMessage.Username}, {username.Antiping()} is not listening to anything";
+                    return;
+                }
+                case SpotifyTrack track:
+                {
+                    string artists = string.Join(", ", track.Artists.Select(a => a.Name));
+                    Response = $"{ChatMessage.Username}, {track.Name} by {artists} || {(track.IsLocal ? "local file" : track.Uri)}";
+                    break;
+                }
+                case SpotifyEpisode episode:
+                {
+                    Response = $"{ChatMessage.Username}, {episode.Name} by {episode.Show.Name} || {(episode.IsLocal ? "local file" : episode.Uri)}";
+                    break;
+                }
+                default:
+                {
+                    Response = $"{ChatMessage.Username}, listening to an unknown Spotify item type monkaS";
+                    break;
+                }
             }
         }).Wait();
     }
