21febd065b92fb495718b070dba0aa47f9cae830
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-06-15 17:05:19 +0200

    :m:

--- OkayegTeaTime.Tools/Program.cs
+++ OkayegTeaTime.Tools/Program.cs
@@ -7,6 +7,11 @@ public static class Program
 {
     private static void Main(string[] args)
     {
+        if (args.Length < 1)
+        {
+            return;
+        }
+
         switch (args[0])
         {
             case "publish":
