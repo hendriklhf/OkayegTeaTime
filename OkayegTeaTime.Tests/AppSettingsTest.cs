5f3a3ec07b4632848621b66d63c320811c86ce73
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-07-06 18:55:45 +0200

    renamed test

---
+++ OkayegTeaTime.Tests/AppSettingsTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using HLE.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OkayegTeaTime.Files;
+
+namespace OkayegTeaTime.Tests;
+
+[TestClass]
+public class AppSettingsTest
+{
+    [TestMethod]
+    public void AllPropertiesNotDefaultTest()
+    {
+        PropertyInfo[] properties = typeof(AppSettings).GetProperties();
+        properties.ForEach(p => Assert.IsTrue(p.GetValue(null, null) != default));
+    }
+}
