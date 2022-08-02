14b35ebaa0798415ba838650c6f8b9c6da7aa393
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-08-02 20:35:32 +0200

    improved FileLocator

--- OkayegTeaTime.Files/FileLocator.cs
+++ OkayegTeaTime.Files/FileLocator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -9,7 +8,7 @@ namespace OkayegTeaTime.Files;
 
 public static class FileLocator
 {
-    private static List<string>? _files;
+    private static string[]? _files;
 
     /// <summary>
     /// Searches for a file in the current and all sub directories and returns the path of it.
@@ -19,27 +18,24 @@ public static class FileLocator
     /// </exception>
     public static string Find(string fileName)
     {
-        if (_files is null)
-        {
-            BuildFileCache();
-        }
-
-        Regex filePattern = new($@"[\/\\]{fileName}$", RegexOptions.Compiled, TimeSpan.FromMilliseconds(250));
-        return _files!.FirstOrDefault(f => filePattern.IsMatch(f)) ?? throw new FileNotFoundException($"The file {fileName} could not be found in the current or any sub directory.");
+        _files ??= BuildFileCache();
+        Regex filePattern = new($@"[\/\\]{fileName}$", RegexOptions.Compiled);
+        return _files.FirstOrDefault(f => filePattern.IsMatch(f)) ?? throw new FileNotFoundException($"The file {fileName} could not be found in the current or any sub directory.");
     }
 
-    private static void BuildFileCache()
+    private static string[] BuildFileCache()
     {
-        _files = new();
-        GetFiles(Directory.GetCurrentDirectory());
+        List<string> files = new();
+        GetFiles(Directory.GetCurrentDirectory(), files);
+        return files.OrderBy(f => f.Count(c => c is '\\' or '/')).ToArray();
     }
 
-    private static void GetFiles(string directory)
+    private static void GetFiles(string directory, List<string> fileList)
     {
         string[] dirs = Directory.GetDirectories(directory);
-        dirs.ForEach(GetFiles);
+        dirs.ForEach(d => GetFiles(d, fileList));
 
         string[] files = Directory.GetFiles(directory);
-        _files?.AddRange(files);
+        fileList.AddRange(files);
     }
 }
