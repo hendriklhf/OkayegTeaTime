742c452c25912b3ce75c6cc5b1ca329bb4740290
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-09-04 22:51:00 +0200

    added ResourceController test

---
+++ OkayegTeaTime.Tests/ResourceTest.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OkayegTeaTime.Resources;
+
+namespace OkayegTeaTime.Tests;
+
+[TestClass]
+public class ResourceTest
+{
+    [TestMethod]
+    public void AllPropertiesNotNullOrEmptyTest()
+    {
+        PropertyInfo[] properties = typeof(ResourceController).GetProperties(BindingFlags.Static | BindingFlags.Public);
+        Assert.IsTrue(properties.Length > 0);
+        foreach (PropertyInfo prop in properties)
+        {
+            string value = (string)prop.GetValue(null);
+            Assert.IsTrue(!string.IsNullOrEmpty(value));
+        }
+    }
+}
